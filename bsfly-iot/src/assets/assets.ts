362ac632c6f0fd2d8ff7a6e62f405a352b55932e
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-15 15:58:15 +0800

    feat(timers): add predefined options for timer selections

--- bsfly-iot/src/assets/assets.ts
+++ bsfly-iot/src/assets/assets.ts
@@ -35,3 +35,27 @@ export const sensorsData = [
     type: "ammonia",
   },
 ];
+
+interface TimeProps {
+  id: number;
+  name: string;
+  seconds: number;
+}
+
+export const timers: TimeProps[] = [
+  {
+    id: 1,
+    name: "Disabled",
+    seconds: 0,
+  },
+  {
+    id: 2,
+    name: "8 Hours",
+    seconds: 28800,
+  },
+  {
+    id: 3,
+    name: "12 Hours",
+    seconds: 43200,
+  },
+];
