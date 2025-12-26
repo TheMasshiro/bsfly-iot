74fba110074704e1633f4f113b38a40b1c25395f
John Christian Vicente <johnc.vicente1@gmail.com>
2025-12-26 11:24:35 +0800

    refactor(toolbar): simplify component by removing conditional rendering

--- bsfly-iot/src/components/Toolbar/Toolbar.tsx
+++ bsfly-iot/src/components/Toolbar/Toolbar.tsx
@@ -1,38 +1,20 @@
-import { IonButton, IonButtons, IonChip, IonIcon, IonMenuButton, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButtons, IonChip, IonMenuButton, IonTitle, IonToolbar } from "@ionic/react";
 import { FC } from "react";
-import ControlModal from "../Modal/Modal";
-import { optionsOutline } from "ionicons/icons";
 
 interface ToolbarProps {
     header: string,
-    isDashboard?: boolean
 }
 
-const Toolbar: FC<ToolbarProps> = ({ header, isDashboard }) => {
+const Toolbar: FC<ToolbarProps> = ({ header }) => {
     return (
         <IonToolbar>
             <IonButtons slot="start">
                 <IonMenuButton />
             </IonButtons>
-            {isDashboard ?
-                <>
-                    <IonTitle>{header}</IonTitle>
-                    <IonChip slot='end' color="danger">
-                        Offline
-                    </IonChip>
-                    <IonButtons slot="end">
-                        <IonButton id="open-control-modal" fill="solid" shape="round" color="secondary">
-                            <IonIcon slot="icon-only" icon={optionsOutline}></IonIcon>
-                            <ControlModal />
-                        </IonButton>
-                    </IonButtons>
-                </>
-                : <>
-                    <IonTitle>{header}</IonTitle>
-                    <IonChip slot='end' color="danger">
-                        Offline
-                    </IonChip>
-                </>}
+            <IonTitle>{header}</IonTitle>
+            <IonChip slot='end' color="danger">
+                Offline
+            </IonChip>
         </IonToolbar>
     )
 }
