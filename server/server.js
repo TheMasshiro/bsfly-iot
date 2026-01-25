8c336ffae39e55a9d26924d46248dba868a4b23b
John Christian Vicente <johnc.vicente1@gmail.com>
2026-01-25 12:11:49 +0800

    refactor(server): Ensure database connection completes before server initialization

--- server/server.js
+++ server/server.js
@@ -11,21 +11,25 @@ const app = express();
 app.use(cors());
 
 const httpServer = createServer();
-await connectDB();
 
 const FRONTEND_PORT = 8100;
 const BACKEND_PORT = 5000;
-const io = new Server(httpServer, {
-  cors: {
-    origin: `http://localhost:${FRONTEND_PORT}`,
-    methods: ["GET", "POST"],
-  },
-});
 
-io.on("connection", (socket) => {
-  registerActuatorHandlers(io, socket);
-});
+(async () => {
+  await connectDB();
 
-httpServer.listen(BACKEND_PORT, () => {
-  console.log(`Server is running on port ${BACKEND_PORT}`);
-});
+  const io = new Server(httpServer, {
+    cors: {
+      origin: `http://localhost:${FRONTEND_PORT}`,
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    registerActuatorHandlers(io, socket);
+  });
+
+  httpServer.listen(BACKEND_PORT, () => {
+    console.log(`Server is running on port ${BACKEND_PORT}`);
+  });
+})();
