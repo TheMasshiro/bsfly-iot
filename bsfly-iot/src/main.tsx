2fd499a122d96c8687885153268aa79813c77b21
John Christian Vicente <johnc.vicente1@gmail.com>
2026-05-10 23:09:07 +0800

    ci(workflow): Securely inject Clerk publishable key into build

--- bsfly-iot/src/main.tsx
+++ bsfly-iot/src/main.tsx
@@ -6,10 +6,6 @@ import { dark } from '@clerk/themes'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-    throw new Error('Missing Publishable Key')
-}
-
 const clerkAppearance = {
     baseTheme: dark,
     variables: {
